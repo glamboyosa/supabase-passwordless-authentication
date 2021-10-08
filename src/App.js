acc748c7a0cc673c6e3df9393f58213558b50a79
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2021-10-08 11:01:48 +0100

    feat: return `Screens`

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import AuthContextProvider from './Context'
 import { createClient } from '@supabase/supabase-js'
 
 import { SUPABASE_URL, SUPABASE_PUBLIC_ANON } from '@env'
+import Screens from './Screens'
 const App = () => {
   const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLIC_ANON)
 
-  return <AuthContextProvider></AuthContextProvider>
+  return (
+    <AuthContextProvider>
+      <Screens />
+    </AuthContextProvider>
+  )
 }
 export default App
 export { supabase }
