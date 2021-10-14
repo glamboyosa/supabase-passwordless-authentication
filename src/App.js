a2ed3eb22e9984ac7504d427f15116ddaed0c12a
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2021-10-14 11:34:43 +0100

    feat: cleanup and added base styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react'
 
-import TruSDK from '@tru_id/tru-sdk-react-native'
-
 import {
   ActivityIndicator,
   Alert,
@@ -25,5 +23,60 @@ const App = () => {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  content: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    marginTop: 10,
+    width: 0.5 * Dimensions.get('window').width,
+    height: 200,
+  },
+  heading: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  textInput: {
+    padding: 15,
+    borderRadius: 3,
+    backgroundColor: '#fff',
+    borderColor: '#858585',
+    borderWidth: 0.4,
+    elevation: 7,
+    marginBottom: 10,
+    shadowColor: '#858585',
+    shadowOffset: { width: 0.5, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 1,
+    color: '#000',
+    width: 0.7 * Dimensions.get('window').width,
+  },
+  spinner: {
+    marginTop: 20,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#1955ff',
+    color: '#fff',
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#1955ff',
+    marginTop: 17,
+    width: 0.35 * Dimensions.get('window').width,
+  },
+  buttonText: {
+    color: '#fff',
+  },
+})
+
 export default App
-export { supabase }
